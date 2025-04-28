201b368e1e5626960cc716baa1a78eca7efa28df
HARSH825 <harshchhallani3@gmail.com>
2025-04-28 18:51:17 +0530

    dele

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import GarageRegistrationForm from "@/components/garage-registration-form"
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-gradient-to-br from-gray-50 to-accent">
-      <GarageRegistrationForm />
-    </main>
-  )
-}
